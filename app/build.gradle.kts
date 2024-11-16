ec6b000f5aceece442ac03837fab2b92d726676a
Maria Luiza <marins190304@gmail.com>
2024-11-16 02:22:49 -0300

    Versão inicial

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("com.android.application")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.olx_clone"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.olx_clone"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    // Dependências do Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+
+    // Dependências específicas do Firebase
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-storage")
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
---
+++ app/src/main/java/com/example/olx_clone/activity/CadastroActivity.java
@@ -0,0 +1,165 @@
+package com.example.olx_clone.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.olx_clone.R;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class CadastroActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+    private FirebaseStorage mStorage;
+
+    // Cadastro e login
+    private Button buttonAcesso;
+    private EditText campoEmail, campoSenha;
+    private Switch tipoAcesso;
+
+    private FirebaseAuth autenticacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cadastro);
+
+        inicializaComponentes();
+
+        buttonAcesso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = campoEmail.getText().toString();
+                String senha = campoSenha.getText().toString();
+
+                // Adicione a lógica para login/cadastro aqui
+
+                if( !email.isEmpty() ){
+                    if( !senha.isEmpty() ){
+
+                        //Verifica estado do switch
+                        if( tipoAcesso.isChecked() ){//Cadastro
+
+                            autenticacao.createUserWithEmailAndPassword(
+                                    email, senha
+                            ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if( task.isSuccessful() ){
+
+                                        Toast.makeText(CadastroActivity.this,
+                                                "Cadastro realizado com sucesso!",
+                                                Toast.LENGTH_SHORT).show();
+
+                                        //Direcionar para a tela principal do App
+
+                                    }else {
+
+                                        String erroExcecao = "";
+
+                                        try{
+                                            throw task.getException();
+                                        }catch (FirebaseAuthWeakPasswordException e){
+                                            erroExcecao = "Digite uma senha mais forte!";
+                                        }catch (FirebaseAuthInvalidCredentialsException e){
+                                            erroExcecao = "Por favor, digite um e-mail válido";
+                                        }catch (FirebaseAuthUserCollisionException e){
+                                            erroExcecao = "Esta conta já foi cadastrada";
+                                        } catch (Exception e) {
+                                            erroExcecao = "Erro ao cadastrar usuário: "  + e.getMessage();
+                                            e.printStackTrace();
+                                        }
+
+                                        Toast.makeText(CadastroActivity.this,
+                                                "Erro: " + erroExcecao ,
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+
+                        }else {//Login
+
+                            autenticacao.signInWithEmailAndPassword(
+                                    email, senha
+                            ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                    if( task.isSuccessful() ){
+
+                                        Toast.makeText(CadastroActivity.this,
+                                                "Logado com sucesso",
+                                                Toast.LENGTH_SHORT).show();
+
+                                    }else {
+                                        Toast.makeText(CadastroActivity.this,
+                                                "Erro ao fazer login: " + task.getException() ,
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            });
+
+                        }
+
+                    }else {
+                        Toast.makeText(CadastroActivity.this,
+                                "Preencha a senha!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }else {
+                    Toast.makeText(CadastroActivity.this,
+                            "Preencha o E-mail!",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        // Configurar a Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Inicializar o Firebase
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+        mStorage = FirebaseStorage.getInstance();
+    }
+
+    private void inicializaComponentes() {
+        campoEmail = findViewById(R.id.editCadastroEmail);
+        campoSenha = findViewById(R.id.editCadastroSenha);
+        buttonAcesso = findViewById(R.id.buttonAcesso);
+        tipoAcesso = findViewById(R.id.switchAcesso);
+        autenticacao = FirebaseAuth.getInstance();
+    }
+}
