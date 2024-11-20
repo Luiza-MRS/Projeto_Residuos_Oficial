0b3e067ccfa00c2929233742cd44401d58783178
ChrisCode-Dev <cristiansouza.dev@gmail.com>
2024-11-20 19:49:13 -0300

    tentativa para colocar um text monetario

--- app/src/main/java/com/example/olx_clone/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/example/olx_clone/activity/CadastrarAnuncioActivity.java
@@ -1,6 +1,8 @@
 package com.example.olx_clone.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +14,24 @@ import com.example.olx_clone.R;
 
 public class CadastrarAnuncioActivity extends AppCompatActivity {
 
+    private EditText campoTitulo, campoDescricao, campoValor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cadastrar_anuncio);
+        incializarComponentes();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void incializarComponentes(){
+        campoTitulo = findViewById(R.id.editTitulo);
+        campoDescricao = findViewById(R.id.editDescricao);
+        campoValor = findViewById(R.id.editValor);
     }
 }
